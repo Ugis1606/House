2f0841a99b78f1ee23cddbb60046305b036ec58e
Ugis Jung <ugisj@elektrons-k.lv>
2024-03-06 15:45:33 +0200

    coment

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,20 +8,43 @@ import javafx.scene.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
+import javafx.scene.shape.Cylinder;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-public class Main extends Application {
+
+public class Main extends Application{
 
     private Parent createContent() {
-        Box box = new Box(5, 5, 5);
+        Sphere moon = new Sphere(1);
+        moon.setMaterial(new PhongMaterial(Color.YELLOW));
+        moon.getTransforms().add(new Translate(0, -6, 40));
+
+        PointLight pointLight = new PointLight();
+        pointLight.setColor(Color.WHITE);
+        pointLight.getTransforms().add(new Translate(0, -20, 40));
+
+        Box house = new Box(5, 5, 5);
         Rectangle ground = new Rectangle(20, 20);
-        box.setMaterial(new PhongMaterial(Color.FIREBRICK));
+        Cylinder tree1 = new Cylinder(1, 4);
+        Cylinder tree2 = new Cylinder(1.2, 6);
+
+        tree1.setTranslateX(7);
+        tree1.setTranslateZ(6);
+        tree1.setTranslateY(-0.3);
+        tree2.setTranslateX(-7);
+        tree2.setTranslateZ(2);
+        tree2.setTranslateY(-0.3);
+
+        house.setMaterial(new PhongMaterial(Color.FIREBRICK));
+        tree1.setMaterial(new PhongMaterial(Color.FORESTGREEN));
+        tree2.setMaterial(new PhongMaterial(Color.FORESTGREEN));
         ground.getTransforms().addAll(  new Rotate(-90, Rotate.X_AXIS),
-                                        new Translate(-ground.getWidth()/2, -ground.getHeight()/2, box.getHeight()/2));
+                                        new Translate(-ground.getWidth()/2, -ground.getHeight()/2, house.getHeight()/2));
         ground.setFill(Color.FORESTGREEN);
 
         Translate pivot = new Translate();
@@ -52,9 +75,7 @@ public class Main extends Application {
 
         // Build the Scene Graph
         Group root = new Group();
-        root.getChildren().add(camera);
-        root.getChildren().add(box);
-        root.getChildren().add(ground);
+        root.getChildren().addAll(camera, house, ground, tree1,tree2, moon);
 
         // Use a SubScene
         SubScene subScene = new SubScene(
